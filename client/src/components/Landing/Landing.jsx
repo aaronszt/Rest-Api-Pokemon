5b0299ec4a023bd90f694c347af099d15a15a990
Aaron Sztychmasjter <110361278+aaronszt@users.noreply.github.com>
2024-01-13 22:45:48 -0300

    some changes

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,18 +1,18 @@
 import style from './Landing.module.css';
-import { useEffect} from 'react';
 import { Link } from 'react-router-dom';
-import audioPoke from '../../assets/audio_pokemon.mpeg';
 import logo from '../../img_pokemon/logo.png'
+// import { useEffect} from 'react';
+// import audioPoke from '../../assets/audio_pokemon.mpeg';
 
 const Landing = () => {
-  useEffect(() => {
-    const audio = new Audio(audioPoke);
-    audio.play();
+//   useEffect(() => {
+//     const audio = new Audio(audioPoke);
+//     audio.play();
 
-    return () => {
-        audio.pause();
-    };
-}, []);
+//     return () => {
+//         audio.pause();
+//     };
+// }, []);
     return (
             <div className={style.meContainer}>
                 <img src = {logo} alt="logo" className = {style.logo}/>
